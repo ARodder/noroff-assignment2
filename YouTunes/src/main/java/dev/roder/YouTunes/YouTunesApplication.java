617392143bbf3eaa95090e0beffa3bbf096be283
Aleksander Røder <fylling8@gmail.com>
2023-01-20 14:31:37 +0100

    initial commit

---
+++ YouTunes/src/main/java/dev/roder/YouTunes/YouTunesApplication.java
@@ -0,0 +1,13 @@
+package dev.roder.YouTunes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouTunesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouTunesApplication.class, args);
+	}
+
+}
---
+++ YouTunes/src/test/java/dev/roder/YouTunes/YouTunesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.roder.YouTunes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouTunesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
