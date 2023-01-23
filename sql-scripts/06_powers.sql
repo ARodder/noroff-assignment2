0078729f7ac0db30ad8b00506d798511f9a26acf
vegardandersson <vegardandersson@outlook.com>
2023-01-23 09:57:10 +0100

    wrote script for inserting four rows into the power database

---
+++ sql-scripts/06_powers.sql
@@ -0,0 +1,8 @@
+
+/* We insert four rows into the power table with only
+ the columns name and description specified, as the primary key
+ id increments automatically because it is of data type serial*/
+insert into power (name, description) values ('Super-stretch', 'Gives you the ability to arch your entire back and stretch rigorously to relieve stress, much like a dog does in the morning');
+insert into power (name, description) values ('Feline instincts', 'Makes you scared of water and gives you the ability to draw upon the powers of catnip in combat');
+insert into power (name, description) values ('Mega-mind', 'Awards you with a grotesquely enlarged cranium that gives you a major advantage when engaged in headbutting bouts');
+insert into power (name, description) values ('Aquatic affinity', 'Makes you only able to consume and engage with water that has a salinity of at least 32‰')
