fae3336da47be012ded77f6fdff131671560d464
Aleksander Røder <fylling8@gmail.com>
2023-01-23 10:28:42 +0100

    add: record for customer, add: customerRepository

---
+++ YouTunes/src/main/java/dev/roder/YouTunes/repositories/CustomerRepository.java
@@ -0,0 +1,39 @@
+package dev.roder.YouTunes.repositories;
+
+import java.util.List;
+
+import dev.roder.YouTunes.models.Customer;
+
+public class CustomerRepository implements CrudRepository<Integer, Customer>{
+
+    @Override
+    public Customer getById(Integer id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<Customer> getAll() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void create(Customer object) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void update(Customer object) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void delete(Integer id) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
