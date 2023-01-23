dd4c9bc8499d0217c05c9b755b4f76d2a7404436
vegardandersson <vegardandersson@outlook.com>
2023-01-23 08:51:35 +0100

    wrote script for creating superhero table

--- sql-scripts/01_tableCreate.sql
+++ sql-scripts/01_tableCreate.sql
@@ -1,7 +1,22 @@
-/* Ensures that the tables does not already exist when we try to
+/* Ensures that the tables do not already exist when we try to
 create them */
+drop table if exists superhero;
 drop table if exists assistant;
 drop table if exists power;
+
+
+/* Creates a table called superhero with a primary key called
+id of type serial(autoincremented integer), name-field of
+type varchar(50) than cannot be null, alias of type varchar(50)
+that cannot be null as well as origin of type varchar(255)
+that cannot be null */
+create table superhero (
+    id serial primary key,
+    name varchar(50) not null,
+    alias varchar(50) not null,
+    origin varchar(255) not null
+);
+
 
 /* Creates a table called assistant with a primary key called
 id of type serial(autoincremented integer), and name-field of
