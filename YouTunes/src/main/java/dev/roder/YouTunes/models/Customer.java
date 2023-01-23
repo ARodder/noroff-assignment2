1bad145a2312ad9aff6f9fd6a109f1d6a54aaa7e
Aleksander Røder <fylling8@gmail.com>
2023-01-23 11:38:56 +0100

    add: read customer by name method, delete: unnecessary DAO-class, reduce: customer to assignment specified fields, change: application run file to use instance of repository-class and not DAO-class

--- YouTunes/src/main/java/dev/roder/YouTunes/models/Customer.java
+++ YouTunes/src/main/java/dev/roder/YouTunes/models/Customer.java
@@ -1,7 +1,8 @@
 package dev.roder.YouTunes.models;
 
 public record Customer(int customerId, String firstName, String lastName,
-        String company, String address, String city, String state, String country, String postalCode, String phone,
-        String fax, String email, int supportRepId) {
+        String address,
+        String city, String country, String postalCode, String phone,
+        String email) {
 
 }
