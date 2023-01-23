cdb8a5013086da7b470fe1a19997d5642605e8d8
Aleksander Røder <fylling8@gmail.com>
2023-01-23 10:09:55 +0100

    add: DAO-class to connect to postgresqldb

--- YouTunes/src/main/java/dev/roder/YouTunes/YouTunesApplication.java
+++ YouTunes/src/main/java/dev/roder/YouTunes/YouTunesApplication.java
@@ -1,13 +1,26 @@
 package dev.roder.YouTunes;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class YouTunesApplication {
+import dev.roder.YouTunes.access.YouTunesDAO;
 
+@SpringBootApplication
+public class YouTunesApplication implements ApplicationRunner{
+
+	@Autowired
+	private YouTunesDAO postgresDb;
 	public static void main(String[] args) {
 		SpringApplication.run(YouTunesApplication.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		// TODO Auto-generated method stub
+		postgresDb.test();
 	}
 
 }
