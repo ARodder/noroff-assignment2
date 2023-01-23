42ff97128ee8e84165cfb739a0e08c16d6cc7401
Aleksander Røder <fylling8@gmail.com>
2023-01-23 12:03:49 +0100

    reduce: customer even further

--- YouTunes/src/main/java/dev/roder/YouTunes/models/Customer.java
+++ YouTunes/src/main/java/dev/roder/YouTunes/models/Customer.java
@@ -1,8 +1,7 @@
 package dev.roder.YouTunes.models;
 
 public record Customer(int customerId, String firstName, String lastName,
-        String address,
-        String city, String country, String postalCode, String phone,
-        String email) {
+                String country, String postalCode, String phone,
+                String email) {
 
 }
