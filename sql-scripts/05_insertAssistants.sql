1e4593a3551eacc3670688b398a54d163c5d3a2e
ARodder <44817628+ARodder@users.noreply.github.com>
2023-02-02 14:10:38 +0100

    Update 05_insertAssistants.sql

--- sql-scripts/05_insertAssistants.sql
+++ sql-scripts/05_insertAssistants.sql
@@ -5,5 +5,5 @@ Only needs name and the id of the superhero the assist
 since the id is automatically created with the serial-type.
 */
 insert into assistant(name, superhero_id) values('Louis Lane',1);
-insert into assistant(name, superhero_id) values('krypto the dog',1);
+insert into assistant(name, superhero_id) values('krypto the dog',2);
 insert into assistant(name, superhero_id) values('vulko',3);
